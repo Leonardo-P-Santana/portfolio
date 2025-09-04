b1217ad913d24d9eee84e6434a08917e3ac738a8
Leonardo-P-Santana <leo_131107@hotmail.com>
2025-09-04 19:17:14 -0300

    Atualiza URL do JSON para a versão online

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,6 @@
 async function fetchProfile() {
-  const url = "/assets/data/profile.json";
+  const url =
+    "https://raw.githubusercontent.com/Leonardo-P-Santana/portfolio/6133e65922046e7cdf1551dcfdefa32792f7ad7f/assets/data/profile.json";
   const fetching = await fetch(url);
   return await fetching.json();
 }
