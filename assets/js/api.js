a92ad4b73f9aa5ac36bcc18d7d1b3bb848189e88
Leonardo-P-Santana <leo_131107@hotmail.com>
2025-09-06 22:35:45 -0300

    Alteração da url

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 async function fetchProfile() {
   const url =
-    "https://raw.githubusercontent.com/Leonardo-P-Santana/portfolio/6133e65922046e7cdf1551dcfdefa32792f7ad7f/assets/data/profile.json";
+    "https://raw.githubusercontent.com/Leonardo-P-Santana/portfolio/refs/heads/main/assets/data/profile.json";
   const fetching = await fetch(url);
   return await fetching.json();
 }
